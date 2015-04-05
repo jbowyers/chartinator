a224b2e73d55288f2cabf7670aee5de9086afeac
jbowyers <jbowyers@gmail.com>
2015-04-05 15:43:55 -0700

    Release v0.3.1

--- src/js/chartinator.js
+++ src/js/chartinator.js
@@ -1,6 +1,6 @@
 /**
  * Chartinator
- * Version: 0.3.0
+ * Version: 0.3.1
  * URL: http://chartinator.com
  * Description: Chartinator transforms data contained in HTML tables, Google Sheets and js arrays into charts using Google Charts
  * Requires: jQuery, Google Charts
